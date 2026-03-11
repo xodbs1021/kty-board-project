3db564d8cd25ddab1ea6bdfe05b6164d099cea06
김태윤 <xodbs9810@gmail.com>
2026-03-12 00:04:35 +0900

    feat: mysql 연결

--- src/main/java/com/kty/board/repository/CommentRepository.java
+++ src/main/java/com/kty/board/repository/CommentRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
     // 특정 게시글에 달린 댓글들만 모아서 보고 싶을 때를 위해 추가
     List<Comment> findByPostId(Long postId);
 }
--- src/main/java/com/kty/board/repository/MemberRepository.java
+++ src/main/java/com/kty/board/repository/MemberRepository.java
@@ -7,5 +7,4 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     // 이메일로 중복 가입 여부를 확인하기 위해 추가
     Optional<Member> findByEmail(String email);
-
 }
